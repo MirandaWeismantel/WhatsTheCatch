f17cfe96966e19d8741e008bbf1c6449f37680cb
Mickey Chao <mjchao2@gmail.com>
2015-02-12 21:40:15 -0500

    Document Image and Sprite

--- Image.py
+++ Image.py
@@ -2,20 +2,96 @@
 Created on Feb 12, 2015
 
 @author: mjchao
-
-A general interface that represents an image to be drawn on the screen.
-It is unchanging
 '''
 
 import pygame
 from pygame.locals import *
 
+'''
+* Represents an Image in the game. While there is nothing preventing one from
+* doing so, Images should be static in position and unchanging in appearance 
+* throughout the game.
+'''
 class Image( pygame.sprite.Sprite ):
     
     '''
-    Draws the image onto the screen. It must be implemented in subclasses
+    * x coordinate of the Image on the screen, in pixels. The x coordinate
+    * is the x value of the top, left corner of the Image.
+    *
+    * This should be
+    * set in the constructor, but never changed afterwards, since the Image is 
+    * static
     '''
-    def draw(self):
+    x = 0
+    
+    '''
+    * y coordinate of the Image on the screen, in pixels. The y coordinate
+    * is the y value of the top, left corner of the Image.
+    *
+    * This should be set in the constructor, but never changed afterwards, 
+    * since the Image is static
+    '''
+    y = 0
+    
+    '''
+    * the width (span in the x direction) of the Image on the screen, in pixels.
+    * This should be set in the constructor, but never changed afterwards,
+    * since the Image is unchanging in appearance
+    '''
+    width = 0
+    
+    '''
+    * the height (span in the y direction) of the Image on the screen, in 
+    * pixels. This should be set in the constructor, but never changed
+    * afterwards, since the Image is unchanging in appearance
+    '''
+    height = 0
+    
+    '''
+    * Draws the image onto the screen. This must be implemented in subclasses.
+    '''
+    def draw( self ):
         pass
+    
+    '''
+    * @return          a pair, [x, y], representing the x and y coordinates of
+    *                  the location of this sprite in pixels.
+    '''
+    def getLocation( self ):
+        return [self.x, self.y]
         
+    '''
+    * Sets the width of the bounding box of this Image. The width is the 
+    * image's span in the x-direction
+    *
+    * @param width            the width of the bounding box of this image,
+    *                         in pixels
+    '''
+    def setWidth( self , width ):
+        self.width = width
+        
+    '''
+    * Sets the height of the bounding box of the Image. The height is the
+    * image's span in the y-direction
+    *
+    * @param height            the height of the bounding box of this image,
+    *                          in pixels
+    '''
+    def setHeight( self , height ):
+        self.height = height
+        
+    '''
+    * @return                 the width of the bounding box of this image,
+    *                         in pixels
+    '''      
+    def getWidth( self ):
+        return self.width
+    
+    '''
+    * @return                 the height of the bounding box of this image,
+    *                         in pixels
+    '''
+    def getHeight( self ):
+        return self.height
+    
     
--- Sprite.py
+++ Sprite.py
@@ -9,52 +9,57 @@ from pygame.locals import *
 
 from Image import Image
 
+'''
+* Represents a Sprite, which is a graphical element capable of 
+* changing in appearance (animation), and is capable of moving around 
+* the screen. This interface provides methods for moving and drawing
+* the Sprite. Examples of Sprites would include Fish, Eel, and FishingLine
+'''
 class Sprite( Image ):
     
-    x = 0
-    y = 0
-    width = 0
-    height = 0
-    
     '''
-    
+    * Draws this sprite onto the screen in the appropriate location and with
+    * the appropriate graphical appearance. This should be overridden
+    * in subclasses.
     '''
-    def draw(self):
+    def draw( self ):
         pass
     
     '''
-    Moves the animation of this sprite forward by 1 frame, 
-    if this sprite can be animated. The frame rate is not handled by this
-    method.
+    * Moves the animation of this sprite forward by 1 frame, 
+    * if this sprite can be animated. The frame rate is not handled by this
+    * method. This method does not have to do anything if the sprite is
+    * not animated.
     '''
-    def animate(self):
+    def animate( self ):
         pass
     
     '''
-    
+    * Sets the sprite's location to the given coordinates
+    *
+    * @param x         the new x coordinates for the sprite, in pixels
+    * @param y         the new y coordinates for the sprite, in pixels
     '''
     def moveTo( self , x , y ):
         self.x = x
         self.y = y
     
-    def getLocation(self):
-        return [self.x, self.y]
-    
+    '''
+    * Moves the sprite by the given amounts in the x and y directions
+    *
+    * @param dx         amount by which to move in the x direction, in pixels
+    * @param dy         amount by which to move in the y direction , in pixels
+    '''
     def move( self , dx , dy ):
         self.x += dx
         self.y += dy
     
+    '''
+    * Handles what happens when this sprite collides with another sprite. 
+    * This method should not be called directly except by the GameWindow, which
+    * will handle collisions at a higher level.
+    *
+    * @param otherSprite     the sprite with which this sprite collided
+    '''
     def onCollide( self , otherSprite ):
         pass
-    
-    def setWidth( self , width ):
-        self.width = width
-        
-    def setHeight( self , height ):
-        self.height = height
-    
-    def getWidth(self):
-        return self.width
-    
-    def getHeight(self):
-        return self.height
