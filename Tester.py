0703396a7249b2fe5b876076f61dcb01d6ea33f1
Mickey Chao <mjchao2@gmail.com>
2015-02-18 15:28:48 -0500

    Prevent fish from being generated off screen
    y range needs to be 200 to 450 not 200 to 500

--- Tester.py
+++ Tester.py
@@ -106,7 +106,7 @@ testSentence = sentence1
     
 def createFish( word ):
     newFish = Fish( word )
-    newFish.moveTo( random.randrange( -200 , 0 ) , random.randrange( 200 , 500 ) )
+    newFish.moveTo( random.randrange( -200 , 0 ) , random.randrange( 200 , 450 ) )
     sprites.append( newFish )
     fishes.append( newFish )
     
