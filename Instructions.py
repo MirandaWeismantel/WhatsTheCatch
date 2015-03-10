8c0233742404f5fbe65973b29dcd15903e313215
Emily Kessler <emkess@umich.edu>
2015-03-09 23:21:34 -0400

    Instructions Page

--- Instructions.py
+++ Instructions.py
@@ -1,5 +1,5 @@
 '''
-Created on Mar 3, 2015
+Created on Mar 9, 2015
 
 @author: emkess
 '''
@@ -7,9 +7,7 @@ Created on Mar 3, 2015
 import pygame 
 from pygame.locals import *
 
-import Tester
-import Instructions 
-
+import Menu
 
 pygame.init()
 pygame.font.init()
@@ -21,7 +19,6 @@ backgroundRect = background.get_rect()
 size = (width, height) = background.get_size()
 screen = pygame.display.set_mode(size)
 
-
 class Button(pygame.sprite.Sprite):
     def __init__(self, color, filename, location):
         pygame.sprite.Sprite.__init__(self)
@@ -31,35 +28,28 @@ class Button(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = location[0]
         self.rect.y = location[1]
-    
-     
-        
-def menu():
 
-    newGame = Button((255,255,255), "res/testButton.png", (50,50))
-    instructions = Button((255,255,255), "res/testButton.png", (100,100))
+def instructions_load():
+    return_button = Button((255,255,255), "res/testButton.png", (50,50))
     
-    state = 0
-    while state == 0:
+    state = 1
+    while( state != 0 ):
         screen.fill([255,255,255])
-        screen.blit(newGame.image, newGame)
+        screen.blit(return_button.image, return_button)
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 state = 1
             if event.type == MOUSEBUTTONDOWN:
                 state = 2
-            
+                
     if (state == 2):
         print("mouse click")
         loc = pygame.mouse.get_pos()
         if (loc[0] > 50 and loc[0] < 90 and loc[1] > 50 and loc[1] < 90):
-            print("main game")
-            Tester.mainGame()
-        elif (loc[0] > 100 and loc[0] < 140 and loc[1] > 100 and loc[1] < 140):
-            print("instructions")
-            Instructions.instructions_load()
-        menu()
+            print("run game")
+            Menu.menu()
+        instructions_load()
             
         
-menu()
+instructions_load()
--- Tester.py
+++ Tester.py
@@ -5,7 +5,7 @@ ATTENTION: If you're grading this, please read README.md first
 '''
 Created on Feb 13, 2015
 
-@author: mjchao, weismant
+@author: mjchao, weismant, emkess
 
 Testing module for graphics
 '''
@@ -23,207 +23,205 @@ from FishingHook import FishingHook
 from Sentence import Word , Blank , Sentence
 from FishingLine import FishingLine
 
-pygame.init()
-pygame.font.init()
-
-background = pygame.image.load("res/background.png")
-backgroundRect = background.get_rect()
-
-size = (width, height) = background.get_size()
-screen = pygame.display.set_mode(size)
-
-#list of images to be drawn
-images = []
-
-#list of sprites to be drawn
-sprites = []
-
-#list of active fish in the ocean
-fishes = []
-
-#list of active eels on the screen
-eels = []
-
-#list of sentences to be used
-sentences = []
-
-#contains the player's score, lives, etc.
-stats = Statistics()
-statsFont = pygame.font.SysFont('Courier New', 15)
-
-def drawStats( screen ):
-    global stats
-    global statsFont
-    livesText = "Lives: " + str( stats.getLives() )
-    text = statsFont.render(livesText , True , (0, 0, 0))
-    screen.blit( text , (400 , 0 ) )
-
-    #player starts with 3 lives
-    stats.addLife()
-    stats.addLife()
-    stats.addLife()
+def mainGame():
+    pygame.init()
+    pygame.font.init()
     
+    background = pygame.image.load("res/background.png")
+    backgroundRect = background.get_rect()
     
-
-    '''
-    THE SENTENCES
-    One day we will make the sentence factory function.
-    But for now we will just have an array of sentences.
-    '''
-
-word1 = Word( "I" )
-blank1 = Blank( [Word( "want" )] , [Word( "Ohio" ) , Word( "Yellow" ) ] )
-word2 = Word( "a" )
-blank2 = Blank( [Word("toy")] , [Word("eat") , Word( "run" ) , Word( "blue" )])
-sentence1 = Sentence( [word1 , blank1 , word2 , blank2 ] , "." )
-
-s2word1 = Word( "A cute" )
-s2blank1 = Blank( [Word( "kitten" ), Word("puppy")] , [Word( "cactus" ) , Word( "jump" ) ] )
-s2word2 = Word( "is a" )
-s2blank2 = Blank( [Word("pet")] , [Word("toy") , Word( "food" ) , Word( "orange" )])
-sentence2 = Sentence( [s2word1 , s2blank1 , s2word2 , s2blank2 ] , "." )
-sentences.append(sentence2)
-
-s3word1 = Word( "Let's go to the" )
-s3blank1 = Blank( [Word( "store" )] , [Word( "ear" ) , Word( "long" ) ] )
-s3word2 = Word( "to" )
-s3blank2 = Blank( [Word("buy"), Word("get")] , [Word("wake") , Word( "flower" ) , Word( "orange" )])
-s3word3 = Word( "some" )
-s3blank3 = Blank( [Word("bread"), Word("milk")] , [Word("hair") , Word( "canyon" )])
-sentence3 = Sentence( [s3word1 , s3blank1 , s3word2 , s3blank2, s3word3, s3blank3 ] , "." )
-sentences.append(sentence3)
-
-s4word1 = Word( "I like to" )
-s4blank1 = Blank( [Word( "give" )] , [Word( "oil" ) , Word( "shirt" ) ] )
-s4word2 = Word( "my friends" )
-s4blank2 = Blank( [Word("hugs"), Word("toys")] , [Word( "9" ) , Word( "rain" )])
-sentence4 = Sentence( [s4word1 , s4blank1 , s4word2 , s4blank2 ] , "." )
-sentences.append(sentence4)
-
-s5word1 = Word( "The snow is" )
-s5blank1 = Blank( [Word( "white" )] , [Word( "ill" ) , Word( "goat" ) ] )
-s5word2 = Word( "and" )
-s5blank2 = Blank( [Word("cold")] , [Word( "far" ) , Word( "up" )])
-sentence5 = Sentence( [s5word1 , s5blank1 , s5word2 , s5blank2 ] , "." )
-sentences.append(sentence5)
-
-endWord = Word( "Congratulations!!" )
-endSentence = Sentence( [endWord] , "." )
-
-'''
-END OF SENTENCES
-'''
-
-#CONTINUE HERE
-#create a random word that will be passed and drawn on the fish
-#first create a random number that will stand for either a word from the incorrect or correct list
-#correctOrIncorrectList = random.randrange(1,2,1)
-#create a random number that will select a word from the appropriate list
-#if (correctOrIncorrectList == 1):
-        #wordNum = random.randrange(1,blank1)
-testSentence = sentence1
+    size = (width, height) = background.get_size()
+    screen = pygame.display.set_mode(size)
     
-def createFish( word ):
-    newFish = Fish( word )
-    newFish.moveTo( random.randrange( -500 , 0 ) , random.randrange( 200 , 450 ) )
-    sprites.append( newFish )
-    fishes.append( newFish )
+    #list of images to be drawn
+    images = []
     
-totalAcceptableFish = 3
-totalUnacceptableFish = 2
-
-def generateFish():
-    global totalAcceptableFish
-    global totalUnacceptableFish
+    #list of sprites to be drawn
+    sprites = []
     
-    if testSentence.isComplete():
-        return
+    #list of active fish in the ocean
+    fishes = []
     
-    numAcceptableFish = 0
-    acceptableWords = testSentence.getAcceptableWordsForNextBlank()
+    #list of active eels on the screen
+    eels = []
     
-    numUnacceptableFish = 0
-    unacceptableWords = testSentence.getUnacceptableWordsForNextBlank()
-    for fish in fishes:
-        for word in testSentence.getAcceptableWordsForNextBlank():
-            if fish.word.equals( word ):
-                numAcceptableFish += 1
-                break
-            for word in testSentence.getUnacceptableWordsForNextBlank():
-                if fish.word.equals( word ):
-                    numUnacceptableFish += 1
+    #list of sentences to be used
+    sentences = []
     
-    while numAcceptableFish < totalAcceptableFish:
-        createFish( acceptableWords[ random.randrange( 0 , len( acceptableWords ) ) ] )
-        numAcceptableFish += 1
+    #contains the player's score, lives, etc.
+    stats = Statistics()
+    statsFont = pygame.font.SysFont('Courier New', 15)
     
-    while numUnacceptableFish < totalUnacceptableFish:
-        createFish( unacceptableWords[ random.randrange( 0 , len( unacceptableWords ) ) ] )
-        numUnacceptableFish += 1
+    def drawStats( screen ):
+        livesText = "Lives: " + str(stats.getLives())
+        text = statsFont.render(livesText , True , (0, 0, 0))
+        screen.blit( text , (400 , 0 ) )
     
-#TODO
-def createNewSentence():
-    global testSentence
-    for fish in fishes:
-        sprites.remove( fish )
-        del fishes[:]
-        testSentence.deleteWords() 
-        testSentence = random.choice(sentences)
-        sentences.remove(testSentence)
-    '''
+        #player starts with 3 lives
+        stats.addLife()
+        stats.addLife()
+        stats.addLife()
+        
+        
+    
+        '''
+        THE SENTENCES
+        One day we will make the sentence factory function.
+        But for now we will just have an array of sentences.
+        '''
+    
     word1 = Word( "I" )
     blank1 = Blank( [Word( "want" )] , [Word( "Ohio" ) , Word( "Yellow" ) ] )
     word2 = Word( "a" )
     blank2 = Blank( [Word("toy")] , [Word("eat") , Word( "run" ) , Word( "blue" )])
-    testSentence = Sentence( [word1 , blank1 , word2 , blank2 ] , "." )
-    '''
-
-#just create all your images and sprites here and add them to the images
-#and sprites list
-testFish = Fish( Word( "want" ) )
-testFish.moveTo(-50, 200)
-sprites.append( testFish )
-fishes.append( testFish )
-
-testBoat = Boat()
-testBoat.moveTo(300, 100)
-sprites.append( testBoat )
-
-testLine = FishingLine(testBoat)
-testLine.moveTo(testBoat.x + 31, 111)
-sprites.append( testLine )
-
-testHook = FishingHook( testBoat, testLine )
-testHook.moveTo(testBoat.x, 180)
-sprites.append( testHook )
-
-testEel = Eel( stats , testHook , testLine )
-testEel.EEL_SPEED = 1.5      #in the future this can be randomized
-testEel.moveTo( -250 , 250 )
-sprites.append( testEel )
-
-testEel2 = Eel( stats , testHook , testLine )
-testEel2.EEL_SPEED = 2
-testEel2.moveTo( -100 , 400 )
-sprites.append( testEel2 )
-
-def createEel():
-    newEel = Eel( stats , testHook , testLine  )
-    newEel.moveTo( -250, 300 ) #TODO Make Random
-    sprites.append( newEel )
-    eels.append( newEel )
-    testEel.EEL_SPEED = 1.5 #TODO Make random (negative and positive)
-
-
-#No need to modify the code below. It just runs the game.
-
-# States:
-# 0 = terminate program
-# 1 = game is running
-# 2 = game is paused
-# 3 = game is over (i.e. player has to start a new game
+    sentence1 = Sentence( [word1 , blank1 , word2 , blank2 ] , "." )
     
-def mainGame():
+    s2word1 = Word( "A cute" )
+    s2blank1 = Blank( [Word( "kitten" ), Word("puppy")] , [Word( "cactus" ) , Word( "jump" ) ] )
+    s2word2 = Word( "is a" )
+    s2blank2 = Blank( [Word("pet")] , [Word("toy") , Word( "food" ) , Word( "orange" )])
+    sentence2 = Sentence( [s2word1 , s2blank1 , s2word2 , s2blank2 ] , "." )
+    sentences.append(sentence2)
+    
+    s3word1 = Word( "Let's go to the" )
+    s3blank1 = Blank( [Word( "store" )] , [Word( "ear" ) , Word( "long" ) ] )
+    s3word2 = Word( "to" )
+    s3blank2 = Blank( [Word("buy"), Word("get")] , [Word("wake") , Word( "flower" ) , Word( "orange" )])
+    s3word3 = Word( "some" )
+    s3blank3 = Blank( [Word("bread"), Word("milk")] , [Word("hair") , Word( "canyon" )])
+    sentence3 = Sentence( [s3word1 , s3blank1 , s3word2 , s3blank2, s3word3, s3blank3 ] , "." )
+    sentences.append(sentence3)
+    
+    s4word1 = Word( "I like to" )
+    s4blank1 = Blank( [Word( "give" )] , [Word( "oil" ) , Word( "shirt" ) ] )
+    s4word2 = Word( "my friends" )
+    s4blank2 = Blank( [Word("hugs"), Word("toys")] , [Word( "9" ) , Word( "rain" )])
+    sentence4 = Sentence( [s4word1 , s4blank1 , s4word2 , s4blank2 ] , "." )
+    sentences.append(sentence4)
+    
+    s5word1 = Word( "The snow is" )
+    s5blank1 = Blank( [Word( "white" )] , [Word( "ill" ) , Word( "goat" ) ] )
+    s5word2 = Word( "and" )
+    s5blank2 = Blank( [Word("cold")] , [Word( "far" ) , Word( "up" )])
+    sentence5 = Sentence( [s5word1 , s5blank1 , s5word2 , s5blank2 ] , "." )
+    sentences.append(sentence5)
+    
+    endWord = Word( "Congratulations!!" )
+    endSentence = Sentence( [endWord] , "." )
+    
+    '''
+    END OF SENTENCES
+    '''
+    
+    #CONTINUE HERE
+    #create a random word that will be passed and drawn on the fish
+    #first create a random number that will stand for either a word from the incorrect or correct list
+    #correctOrIncorrectList = random.randrange(1,2,1)
+    #create a random number that will select a word from the appropriate list
+    #if (correctOrIncorrectList == 1):
+            #wordNum = random.randrange(1,blank1)
+    testSentence = sentence1
+        
+    def createFish( word ):
+        newFish = Fish( word )
+        newFish.moveTo( random.randrange( -500 , 0 ) , random.randrange( 200 , 450 ) )
+        sprites.append( newFish )
+        fishes.append( newFish )
+        
+    totalAcceptableFish = 3
+    totalUnacceptableFish = 2
+    
+    def generateFish():
+        global totalAcceptableFish
+        global totalUnacceptableFish
+        
+        if testSentence.isComplete():
+            return
+        
+        numAcceptableFish = 0
+        acceptableWords = testSentence.getAcceptableWordsForNextBlank()
+        
+        numUnacceptableFish = 0
+        unacceptableWords = testSentence.getUnacceptableWordsForNextBlank()
+        for fish in fishes:
+            for word in testSentence.getAcceptableWordsForNextBlank():
+                if fish.word.equals( word ):
+                    numAcceptableFish += 1
+                    break
+                for word in testSentence.getUnacceptableWordsForNextBlank():
+                    if fish.word.equals( word ):
+                        numUnacceptableFish += 1
+        
+        while numAcceptableFish < totalAcceptableFish:
+            createFish( acceptableWords[ random.randrange( 0 , len( acceptableWords ) ) ] )
+            numAcceptableFish += 1
+        
+        while numUnacceptableFish < totalUnacceptableFish:
+            createFish( unacceptableWords[ random.randrange( 0 , len( unacceptableWords ) ) ] )
+            numUnacceptableFish += 1
+        
+    #TODO
+    def createNewSentence():
+        global testSentence
+        for fish in fishes:
+            sprites.remove( fish )
+            del fishes[:]
+            testSentence.deleteWords() 
+            testSentence = random.choice(sentences)
+            sentences.remove(testSentence)
+        '''
+        word1 = Word( "I" )
+        blank1 = Blank( [Word( "want" )] , [Word( "Ohio" ) , Word( "Yellow" ) ] )
+        word2 = Word( "a" )
+        blank2 = Blank( [Word("toy")] , [Word("eat") , Word( "run" ) , Word( "blue" )])
+        testSentence = Sentence( [word1 , blank1 , word2 , blank2 ] , "." )
+        '''
+    
+    #just create all your images and sprites here and add them to the images
+    #and sprites list
+    testFish = Fish( Word( "want" ) )
+    testFish.moveTo(-50, 200)
+    sprites.append( testFish )
+    fishes.append( testFish )
+    
+    testBoat = Boat()
+    testBoat.moveTo(300, 100)
+    sprites.append( testBoat )
+    
+    testLine = FishingLine(testBoat)
+    testLine.moveTo(testBoat.x + 31, 111)
+    sprites.append( testLine )
+    
+    testHook = FishingHook( testBoat, testLine )
+    testHook.moveTo(testBoat.x, 180)
+    sprites.append( testHook )
+    
+    testEel = Eel( stats , testHook , testLine )
+    testEel.EEL_SPEED = 1.5      #in the future this can be randomized
+    testEel.moveTo( -250 , 250 )
+    sprites.append( testEel )
+    
+    testEel2 = Eel( stats , testHook , testLine )
+    testEel2.EEL_SPEED = 2
+    testEel2.moveTo( -100 , 400 )
+    sprites.append( testEel2 )
+    
+    def createEel():
+        newEel = Eel( stats , testHook , testLine  )
+        newEel.moveTo( -250, 300 ) #TODO Make Random
+        sprites.append( newEel )
+        eels.append( newEel )
+        testEel.EEL_SPEED = 1.5 #TODO Make random (negative and positive)
+    
+    
+    #No need to modify the code below. It just runs the game.
+    
+    # States:
+    # 0 = terminate program
+    # 1 = game is running
+    # 2 = game is paused
+    # 3 = game is over (i.e. player has to start a new game
+    
     state = 1
     while( state != 0 ):
     
@@ -286,5 +284,5 @@ def mainGame():
             if event.type == pygame.QUIT:
                 state = 0
             
-            
-        
+if(__name__ == "__main__"):
+    mainGame()           
