aeaf1bb2a868437dc98abed7502cb829e35d6a68
jacobmyers <myersjac@umich.edu>
2015-03-26 08:04:07 -0400

    Button update
    buttons do not look distorted anymore

--- Menu.py
+++ Menu.py
@@ -28,7 +28,7 @@ instructionsBox = 0
 class Button(pygame.sprite.Sprite):
     def __init__(self, color, filename, location):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(filename).convert()
+        self.image = pygame.image.load(filename).convert_alpha()
         self.image.set_colorkey(color) 
         self.image = pygame.transform.scale(self.image, (100,40))
         self.rect = self.image.get_rect()
