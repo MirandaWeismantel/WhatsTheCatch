fb3ad89d856fb297dde22e551d6ac7e1ccd59251
Mickey Chao <mjchao2@gmail.com>
2015-04-07 13:27:15 -0400

    Simplify Menu, Waiting for Jake's button images

--- Menu.py
+++ Menu.py
@@ -25,19 +25,27 @@ screen = pygame.display.set_mode(size)
 instructionsBox = 0
         
 def menu():
-    PLAY_BUTTON_LOCATION = (100, 240)
-    INSTRUCTION_BUTTON_LOCATION = (300, 240)
-    newGame = Button((255,255,255), "Buttons/PlayButton.png", (PLAY_BUTTON_LOCATION))
-    instructions = Button((255,255,255), "Buttons/InstructionButton.png", (INSTRUCTION_BUTTON_LOCATION))
+    continueGame = Button( (255,255,255) , "Buttons/PlayButton.png" , (200 , 200) )
+    newGame = Button((255,255,255), "Buttons/PlayButton.png", (200, 250))
+    instructions = Button((255,255,255), "Buttons/InstructionButton.png", (200, 300))
+    sentences = Button( (255,255,255) , "Buttons/PlayButton.png" , (200 , 350) )
+    customizations = Button( (255,255,255) , "Buttons/PlayButton.png" , (200,400) )
+    quit = Button( (255,255,255) , "Buttons/PlayButton.png" , (200,450) )
     
     Tester.restart()
     
     state = 0
     while state == 0:
+        
         screen.fill([255,255,255])
         screen.blit(background, backgroundRect)
+        screen.blit( continueGame.image , continueGame )
         screen.blit(newGame.image, newGame)
         screen.blit(instructions.image, instructions)
+        screen.blit( sentences.image , sentences )
+        screen.blit( customizations.image , customizations )
+        screen.blit( quit.image , quit )
+        
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
@@ -45,11 +53,22 @@ def menu():
             if event.type == MOUSEBUTTONDOWN:
                 print("mouse click")
                 loc = pygame.mouse.get_pos()
-                if (newGame.clicked( loc[ 0 ] , loc[ 1 ] ) ):
+                if (continueGame.clicked( loc[ 0 ] , loc[ 1 ] ) ):
+                    Tester.resume()
+                elif (newGame.clicked( loc[ 0 ] , loc[ 1 ] ) ):
                     print("main game")
+                    Tester.restart()
                     Tester.resume()
                 elif ( instructions.clicked( loc[ 0 ] , loc[ 1 ] ) ):
                     print("instruction menu")
                     Instructions.load()
+                elif( sentences.clicked( loc[ 0 ] , loc[ 1 ] ) ):
+                    #TODO
+                    pass
+                elif( customizations.clicked( loc[ 0 ] , loc[ 1 ] ) ):
+                    #TODO
+                    pass
+                elif ( quit.clicked( loc[ 0 ] , loc[ 1 ] ) ):
+                    return
             
 menu()
